4f624fe463e26d29979aa21b7e3a692d11395ac7
Terje Torkelsen <tertork@gmail.com>
2019-11-12 00:01:33 +0100

    Fix error in output and typo in template

--- outputs.tf
+++ outputs.tf
@@ -11,9 +11,9 @@ output "hub_ids" {
 output "keys" {
   description = "Map of hubs with keys => primary_key / secondary_key mapping."
   sensitive   = true
-  value = { for k, h in local.keys : h.key.name => {
-    primary_key   = azurerm_eventhub_authorization_rule.events[k].primary_key
-    secondary_key = azurerm_eventhub_authorization_rule.events[k].secondary_key
+  value = { for k, h in azurerm_eventhub_authorization_rule.events : h.name => {
+    primary_key   = h.primary_key
+    secondary_key = h.secondary_key
     }
   }
 }
