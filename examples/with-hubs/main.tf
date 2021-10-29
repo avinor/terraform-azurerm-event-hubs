52c8d1c34ed296d951bc5e295ff6975f2b44e479
Øyvind Strømmen <insula.ventus@gmail.com>
2021-10-29 10:25:39 +0200

    Run terraform fmt

--- examples/with-hubs/main.tf
+++ examples/with-hubs/main.tf
@@ -1,33 +1,33 @@
 module "simple" {
-    source = "../../"
+  source = "../../"
 
-    name = "simple"
-    location = "westeurope"
-    resource_group_name = "events-simple-rg"
-    sku = "Standard"
-    capacity = 1
+  name                = "simple"
+  location            = "westeurope"
+  resource_group_name = "events-simple-rg"
+  sku                 = "Standard"
+  capacity            = 1
 
-    hubs = [
+  hubs = [
+    {
+      name              = "input"
+      partitions        = 8
+      message_retention = 1
+      consumers = [
+        "app1",
+        "app2"
+      ]
+      keys = [
         {
-            name = "input"
-            partitions = 8
-            message_retention = 1
-            consumers = [
-                "app1",
-                "app2"
-            ]
-            keys = [
-                {
-                    name = "app1"
-                    listen = true
-                    send = false
-                },
-                {
-                    name = "app2"
-                    listen = true
-                    send = true
-                }
-            ]
+          name   = "app1"
+          listen = true
+          send   = false
+        },
+        {
+          name   = "app2"
+          listen = true
+          send   = true
         }
-    ]
+      ]
+    }
+  ]
 }
--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ terraform {
   }
 }
 
-provider azurerm {
+provider "azurerm" {
   features {}
 }
 
